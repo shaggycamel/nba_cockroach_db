82206f2a79d09e954dfb82f2f14b1c0c90d4f28e
Oliver Eaton <eat_fred@protonmail.com>
2023-07-25 11:09:05 +0300

    cleaned dir

--- .Rprofile
+++
@@ -1,4 +0,0 @@
-
-# 131072 is the orginal connection size, so it is being doubled in this instance
-Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
-
--- delete_update/attempt.R
+++
@@ -1,8 +0,0 @@
-
-# To install packages:
-# First navigate to /opt/homebrew/bin
-# Secondly, run pip command
-library(reticulate)
-
-source_python(here::here("data_eventually_delete", "nba_api_calls", "player_season_stats.py"))
-py_run_string("get_player_season_stats('te')")
