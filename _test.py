0a972446593dc15bf6147d1b31b814bb2a64dd29
Oliver Eaton <eat_fred@protonmail.com>
2026-09-18 20:17:04 +1200

    general push

--- _test.py
+++ _test.py
@@ -8,7 +8,7 @@ leagues = (
     hub.db.read(f"select * from fty.customer_league where season = '{hub.ctx.cur_season}'")
     .group_by('league_id')
     .first(ignore_nulls=True)
-    # .filter(~pl.col('league_id').is_in([95537, 1966813226, 24608])) # Remove
+    # .filter(~pl.col('league_id').is_in([95537, 1440301458, 1966813226])) # Remove
 )
 
 hub.fty.connect_leagues(leagues=leagues)
@@ -16,5 +16,11 @@ hub.fty.connect_leagues(leagues=leagues)
 # Done manually:
 # League matchup dates
 # League byes
+
+
+hub.fty.get_league()
+hub.fty.get_league_categories()
+hub.fty.get_league_competitor()
+hub.fty.get_league_matchup()
 
 
